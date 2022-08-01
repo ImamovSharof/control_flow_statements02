f14334bf2acdb3ac290841153ae3eee3273d86b3
sharof <sharofimamov3@gmail.com>
2022-08-01 11:18:31 +0500

    06

--- bin/if06.dart
+++ bin/if06.dart
@@ -7,15 +7,16 @@ Returns:
     int: return answer. 
 */
 int func(int a){
-    
+    // 76514
            // 12345
     int x1=a%10;      //5
     int x2=a~/10%10;    //4
     int x3=a~/10~/10%10;  //3
-    int x4=a~/10~/10%10;   //2
+    int x4=a~/10~/10~/10%10;   //2
     int x5=a~/10~/10~/10~/10;  //1
      
     if(x1>x2){
+        
         return 1;
     }
     else if (x2>x3){
@@ -27,13 +28,13 @@ int func(int a){
     else if (x4>x5){
         return 4;
     }
-    return 5;
+     return 5;
     
     
 }
 
 
 void main() {
-    print(func(45945));
+    print(func(76514));
 }
 
