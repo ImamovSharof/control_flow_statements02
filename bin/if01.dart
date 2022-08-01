daef74ae8c50e28a4013f77a83ad32daf71492a9
sharof <sharofimamov3@gmail.com>
2022-08-01 09:21:24 +0500

    01

--- bin/if01.dart
+++ bin/if01.dart
@@ -8,5 +8,23 @@ Args:
 Returns:
     int: return answer.
 */
-void main() {
+int func (int a, int b, int c){
+    if(a>b){
+        if(a>c){
+              return a;
+        }
+    }
+       else if(b>a){
+            if(b>c){
+             return b;
+        }
+    }
+     return c;
+
+}     
+
+
+void main() 
+{
+print(func(1,4,2));
 }
